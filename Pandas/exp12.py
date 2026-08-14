c33a8888a078d20f9fa48141683f93ec643b116d
sreeharicsunil <sreeharic.sunil@gmail.com>
2026-08-14 15:26:38 +0530

    Initial commit

--- Pandas/exp12.py
+++ Pandas/exp12.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from pandas import date_range
 
-sr = pd.Series(pd,date_range('2021-05-01','2021-05-12',freq = 'D'))
+sr = pd.Series(pd.date_range('2021-05-01','2021-05-12',freq = 'D'))
 print(sr.to_string(index=False))
